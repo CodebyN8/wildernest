40a79b214fc2434b64808714b8ac1231d5ca5cde
codebyn8 <codebyn8@gmail.com>
2025-01-04 18:58:42 -0500

    Added onDelete cascade to few models to try and fix a bug

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
 
     static associate(models) {
       // define association here
-      Spot.belongsTo(models.User, { foreignKey: "ownerId", as: "Owner" });
+      Spot.belongsTo(models.User, {
+        foreignKey: "ownerId",
+        onDelete: "CASCADE",
+      });
       Spot.hasMany(models.Booking, {
         foreignKey: "spotId",
         onDelete: "CASCADE",
