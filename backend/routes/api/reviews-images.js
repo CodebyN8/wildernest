f277c70726f67e2bbf9e230e813d40c506d8ad0f
codebyn8 <codebyn8@gmail.com>
2025-01-06 17:58:24 -0500

    delete review images

--- backend/routes/api/reviews-images.js
+++ backend/routes/api/reviews-images.js
@@ -4,36 +4,40 @@ const { requireAuth } = require("../../utils/auth");
 
 const router = express.Router();
 
-//Delete a review image
-router.delete("/:reviewId/images/:imageId", requireAuth, async (req, res) => {
+// Delete a review image
+router.delete("/:imageId", requireAuth, async (req, res, next) => {
   const { imageId } = req.params;
-  const user = req.user.id;
+  const userId = req.user.id;
+
   const image = await ReviewImage.findOne({
-    where: {
-      id: imageId,
-    },
+    where: { id: imageId },
   });
 
-  if (image === null) {
+  if (!image) {
     return res.status(404).json({
       message: "Review Image couldn't be found",
     });
   }
 
   const review = await Review.findOne({
-    where: {
-      id: image.reviewId,
-    },
+    where: { id: image.reviewId },
   });
 
-  if (user !== review.userId) {
-    return res.status(401).json({
+  if (!review) {
+    return res.status(404).json({
+      message: "Review associated with the image couldn't be found",
+    });
+  }
+
+  if (review.userId !== userId) {
+    return res.status(403).json({
       message: "Review Image does not belong to user",
     });
   }
 
-  image.destroy();
-  res.json({ message: "Successfully deleted" });
+  await image.destroy();
+
+  return res.json({ message: "Successfully deleted" });
 });
 
 module.exports = router;
