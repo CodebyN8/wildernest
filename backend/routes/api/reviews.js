5783ff369501a4e5d31893be21babaf19277a9af
codebyn8 <codebyn8@gmail.com>
2025-01-05 21:30:13 -0500

    added route for adding image to a review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -11,4 +11,47 @@ const { requireAuth } = require("../../utils/auth");
 
 const router = express.Router();
 
+//add image to review based on id
+router.post("/:reviewId/images", requireAuth, async (req, res) => {
+  console.log("hello");
+  const { reviewId } = req.params;
+  const { url } = req.body;
+  console.log("test");
+
+  const reviews = await Review.findOne({
+    where: {
+      id: reviewId,
+    },
+  });
+
+  if (reviews === null) {
+    res.status(404).json({
+      message: "Review couldn't be found",
+    });
+  }
+
+  const imageCount = await ReviewImage.count({
+    where: {
+      reviewId: reviewId,
+    },
+  });
+
+  if (imageCount >= 10) {
+    res.status(403).json({
+      message: "Maximum number of images for this resource was reached",
+    });
+  }
+
+  console.log(reviews);
+
+  const image = await ReviewImage.create({ reviewId: reviews.id, url });
+
+  const resImage = {
+    id: image.id,
+    url: image.url,
+  };
+
+  res.json(resImage);
+});
+
 module.exports = router;
