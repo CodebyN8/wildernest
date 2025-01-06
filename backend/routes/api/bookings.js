c2d9d85d57a42b9efd6f48a32314abbf3af31804
codebyn8 <codebyn8@gmail.com>
2025-01-06 17:10:03 -0500

    edit a booking

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,5 +1,165 @@
 const express = require("express");
-const { Spot, Booking } = require("../models"); // Adjust the path as needed
+
+const {
+  User,
+  Spot,
+  SpotImage,
+  Review,
+  Sequelize,
+  Booking,
+  ReviewImage,
+} = require("../../db/models");
+const { Op } = require("sequelize");
+const { requireAuth } = require("../../utils/auth");
+const spot = require("../../db/models/spot");
+
 const router = express.Router();
+
+//edit a booking
+router.put("/:bookingId", requireAuth, async (req, res) => {
+  const { bookingId } = req.params;
+
+  const { startDate, endDate } = req.body;
+  const errors = {};
+
+  const bookingFields = ["startDate", "endDate"];
+  let currentDate = new Date();
+  const booking = await Booking.findOne({
+    where: { id: bookingId },
+  });
+
+  if (!booking) {
+    res.status(404).json({
+      message: "Booking couldn't be found",
+    });
+  }
+
+  if (startDate > endDate) {
+    return res
+      .status(400)
+      .json({ message: "endDate cannot be on or before startDate" });
+  }
+
+  const conflictingBookings = await Booking.findAll({
+    where: {
+      spotId: booking.spotId,
+      [Op.or]: [
+        {
+          startDate: {
+            [Op.lte]: startDate,
+          },
+          endDate: {
+            [Op.between]: [startDate, endDate],
+          },
+        },
+        {
+          startDate: {
+            [Op.between]: [startDate, endDate],
+          },
+          endDate: {
+            [Op.gte]: endDate,
+          },
+        },
+        {
+          startDate: {
+            [Op.between]: [startDate, endDate],
+          },
+          endDate: {
+            [Op.between]: [startDate, endDate],
+          },
+        },
+        {
+          startDate: {
+            [Op.lte]: startDate,
+          },
+          endDate: {
+            [Op.gte]: endDate,
+          },
+        },
+      ],
+    },
+  });
+
+  if (conflictingBookings.length > 0) {
+    errors.startDate = "Start date conflicts with an existing booking";
+    errors.endDate = "End date conflicts with an existing booking";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return res.status(403).json({
+      message: "Sorry, this spot is already booked for the specified dates",
+      errors,
+    });
+  }
+
+  if (new Date(booking.startDate) < currentDate) {
+    return res.json({
+      message: "Past bookings can't be modified",
+    });
+  }
+
+  if (new Date(startDate) < currentDate || new Date(endDate) < currentDate) {
+    return res.json({
+      message: "You cannot set a booking in the past",
+    });
+  }
+
+  if (new Date(booking.startDate) < currentDate) {
+    return res.json({
+      message: "Past bookings can't be modified",
+    });
+  }
+
+  bookingFields.forEach((field) => {
+    if (req.body[field]) {
+      booking[field] = req.body[field];
+    }
+  });
+  const newstartDate = new Date(booking.startDate);
+  const newendDate = new Date(booking.endDate);
+
+  if (newendDate < newstartDate) {
+    return res
+      .status(400)
+      .json({ error: "End date must be after the start date." });
+  }
+
+  booking.save();
+
+  res.json({ booking });
+});
+
+//delete a booking
+router.delete("/:bookingId", requireAuth, async (req, res) => {
+  const { bookingId } = req.params;
+  const user = req.user.id;
+  const currentDate = new Date();
+  const booking = await Booking.findOne({
+    where: {
+      id: bookingId,
+    },
+  });
+
+  if (booking === null) {
+    return res.status(404).json({
+      message: "Booking couldn't be found",
+    });
+  }
+
+  if (booking.userId !== user) {
+    return res.status(403).json({
+      message: "Booking does not belong to user",
+    });
+  }
+
+  if (currentDate > booking.startDate) {
+    return res.json({
+      message: "Bookings that have been started can't be deleted",
+    });
+  }
+
+  booking.destroy();
+  res.json({ message: "Successfully deleted" });
+});
 
 module.exports = router;
